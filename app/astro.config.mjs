feea0cba2d1a036a441f17c74f15cb735e66dfd7
Xander Salathe <xsalathe@gmail.com>
2024-08-02 20:22:24 +1000

    feat: add client side filtering of JSON list of coffee roasters

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), react(), preact()]
 });
