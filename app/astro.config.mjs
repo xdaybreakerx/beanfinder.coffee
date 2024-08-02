f2dd3a15fc08643f49d9a5689131ee128f34281a
Xander Salathe <xsalathe@gmail.com>
2024-08-02 20:30:24 +1000

    feat: add netlify deployment files

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -4,7 +4,11 @@ import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import preact from "@astrojs/preact";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), react(), preact()]
+  integrations: [tailwind(), sitemap(), react(), preact()],
+  output: "server",
+  adapter: netlify()
 });
