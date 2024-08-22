d3113cca894b6e64d48acd623220507ca8ebba55
Xander Salathe <xsalathe@gmail.com>
2024-08-22 11:54:27 +1000

    fix: remove preact to resolve conflicts with React and third-party libraries

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -2,20 +2,21 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
-import preact from "@astrojs/preact";
 import netlify from "@astrojs/netlify";
 
 // Determine if we're in a production environment
 const isProduction = process.env.NODE_ENV === 'production';
 
 // Use different site URLs based on the environment
-const siteUrl = isProduction
-  ? 'https://www.beanfinder.coffee'  // Production site URL
-  : 'http://localhost:4321';         // Local development URL
+const siteUrl = isProduction ? 'https://www.beanfinder.coffee' // Production site URL
+: 'http://localhost:4321'; // Local development URL
 
+
+// https://astro.build/config
 export default defineConfig({
-  site: siteUrl,                      // Use the dynamically selected site URL
-  integrations: [tailwind(), sitemap(), react(), preact()],
+  site: siteUrl,
+  // Use the dynamically selected site URL
+  integrations: [tailwind(), sitemap(), react()],
   output: "server",
   adapter: netlify()
 });
