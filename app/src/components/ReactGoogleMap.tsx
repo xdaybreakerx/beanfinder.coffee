1837671f963034cab964356d1f0f2129f19adf4b
Xander Salathe <xsalathe@gmail.com>
2024-08-23 19:57:01 +1000

    refactor(map): remove useSearchGoogleMap hook to prevent race condition
    Refactored MonolithicGoogleMap component to handle place search directly within the component, replacing the useSearchGoogleMap hook. This change addresses a race condition that occurred due to Astro's partial hydration process.

--- app/src/components/ReactGoogleMap.tsx
+++ app/src/components/ReactGoogleMap.tsx
@@ -1,55 +1,65 @@
-import React, { useState } from "react";
-import { APIProvider, Map, useMap } from "@vis.gl/react-google-maps";
-import { useGeolocation } from "../hooks/useGeolocation";
-import { usePoiCreation } from "../hooks/usePoiCreation";
-import { useMarkers } from "../hooks/useMarkers";
-import { useMapLoading } from "../hooks/useMapLoading";
-import { useSearchGoogleMap } from "../hooks/useSearchGoogleMap";
-import PlaceOverviewComponent from "./PlaceOverviewComponent";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  APIProvider,
+  ControlPosition,
+  MapControl,
+  Map,
+  useMap,
+  useMapsLibrary,
+  useAdvancedMarkerRef,
+  AdvancedMarker,
+} from "@vis.gl/react-google-maps";
 
-const ReactGoogleMap = ({ apiKey, searchLocation }) => {
+import PlaceOverviewComponent from "./PlaceOverviewComponent";
+import { useMapLoading } from "../hooks/useMapLoading";
+import { useGeolocation } from "../hooks/useGeolocation";
+import { useMarkers } from "../hooks/useMarkers";
+import { usePoiCreation } from "../hooks/usePoiCreation";
+
+const MonolithicGoogleMap = ({ apiKey }) => {
   const mapLoaded = useMapLoading(apiKey);
   const roastersPois = usePoiCreation();
   const { location: userLocation } = useGeolocation();
+
   const [selectedPlaceId, setSelectedPlaceId] = useState<string | null>(null);
-  const { inputRef } = useSearchGoogleMap(apiKey);
+  const [selectedPlace, setSelectedPlace] =
+    useState<google.maps.places.PlaceResult | null>(null);
+
+  const [markerRef, marker] = useAdvancedMarkerRef();
 
   if (!mapLoaded) {
-    return <div>Loading map...</div>;
+    return <div>Loading...</div>;
   }
 
   const handleMarkerClick = (placeId: string) => {
+    console.log("ReactGoogleMap: Marker clicked, placeId:", placeId);
     setSelectedPlaceId(placeId);
   };
 
   return (
     <APIProvider apiKey={apiKey}>
       <div className="relative">
-        {/* Floating Search Bar */}
-        <input
-          ref={inputRef}
-          type="text"
-          placeholder="Search for a location"
-          className="absolute top-4 left-1/2 transform -translate-x-1/2 z-10 bg-white p-2 rounded-lg shadow-lg w-3/4"
-        />
-        {/* Map Instance */}
+        {/* Custom Search Input */}
+        <PlaceAutocomplete onPlaceSelect={setSelectedPlace} />
+
+        {/* Map Configuration */}
         <Map
           style={{ width: "100%", height: "500px" }}
           defaultCenter={
-            searchLocation ||
             userLocation || {
               lat: -24.670940951770845,
               lng: 134.52585021148653,
             }
           }
-          defaultZoom={searchLocation || userLocation ? 10 : 3}
+          defaultZoom={userLocation ? 10 : 3}
           gestureHandling={"greedy"}
           disableDefaultUI={true}
           mapId="7b1c394057aa4afc"
         >
-          {/* Place of Interest Markers */}
+          <AdvancedMarker ref={markerRef} position={null} />
           <PoiMarkers pois={roastersPois} onMarkerClick={handleMarkerClick} />
         </Map>
+        <MapHandler place={selectedPlace} marker={marker} />
         {selectedPlaceId && (
           <PlaceOverviewComponent apiKey={apiKey} placeId={selectedPlaceId} />
         )}
@@ -58,10 +68,74 @@ const ReactGoogleMap = ({ apiKey, searchLocation }) => {
   );
 };
 
-const PoiMarkers = ({ pois, onMarkerClick }) => {
+interface MapHandlerProps {
+  place: google.maps.places.PlaceResult | null;
+  marker: google.maps.marker.AdvancedMarkerElement | null;
+}
+
+const MapHandler = ({ place, marker }: MapHandlerProps) => {
   const map = useMap();
-  useMarkers(map, pois, onMarkerClick);
+
+  useEffect(() => {
+    if (!map || !place || !marker) return;
+
+    if (place.geometry?.viewport) {
+      map.fitBounds(place.geometry?.viewport);
+    }
+    marker.position = place.geometry?.location;
+  }, [map, place, marker]);
+
   return null;
 };
 
-export default ReactGoogleMap;
+interface PlaceAutocompleteProps {
+  onPlaceSelect: (place: google.maps.places.PlaceResult | null) => void;
+}
+
+const PlaceAutocomplete = ({ onPlaceSelect }: PlaceAutocompleteProps) => {
+  const [placeAutocomplete, setPlaceAutocomplete] =
+    useState<google.maps.places.Autocomplete | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const places = useMapsLibrary("places");
+
+  useEffect(() => {
+    if (!places || !inputRef.current) return;
+
+    const options = {
+      fields: ["geometry", "name", "formatted_address"],
+      componentRestrictions: { country: "au" }, // Restrict search to Australia
+    };
+
+    setPlaceAutocomplete(new places.Autocomplete(inputRef.current, options));
+  }, [places]);
+
+  useEffect(() => {
+    if (!placeAutocomplete) return;
+
+    placeAutocomplete.addListener("place_changed", () => {
+      onPlaceSelect(placeAutocomplete.getPlace());
+    });
+  }, [onPlaceSelect, placeAutocomplete]);
+
+  return (
+    <input
+      ref={inputRef}
+      type="text"
+      placeholder="Search for a location"
+      className="absolute top-4 left-1/2 transform -translate-x-1/2 z-10 bg-white p-2 rounded-lg shadow-lg w-3/4"
+      onFocus={() => console.log("ReactGoogleMap: Search input focused")}
+    />
+  );
+};
+
+const PoiMarkers = ({ pois, onMarkerClick }) => {
+  const map = useMap();
+
+  console.log("PoiMarkers: useMap result map =", map);
+
+  useMarkers(map, pois, onMarkerClick);
+
+  return null;
+};
+
+export default MonolithicGoogleMap;
