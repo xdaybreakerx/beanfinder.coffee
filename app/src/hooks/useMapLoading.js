91effd25e4572516eef9a151476ec8bbc2bf3ae9
Xander Salathe <xsalathe@gmail.com>
2024-08-23 20:46:05 +1000

    fix: formatting changes

--- app/src/hooks/useMapLoading.js
+++ app/src/hooks/useMapLoading.js
@@ -5,9 +5,24 @@ export function useMapLoading(apiKey) {
   const [mapLoaded, setMapLoaded] = useState(false);
 
   useEffect(() => {
-    loadGoogleMaps(apiKey).then(() => {
-      setMapLoaded(true);
-    });
+    console.log("useMapLoading: Attempting to load Google Maps API");
+
+    const loadMaps = async () => {
+      try {
+        await loadGoogleMaps(apiKey);
+        console.log("useMapLoading: Google Maps API successfully loaded");
+        setMapLoaded(true);
+      } catch (error) {
+        console.error("useMapLoading: Error loading Google Maps API", error);
+        setMapLoaded(false);  // handle the error by setting mapLoaded to false
+      }
+    };
+
+    if (apiKey) {
+      loadMaps();
+    } else {
+      console.error("useMapLoading: API key is missing");
+    }
   }, [apiKey]);
 
   return mapLoaded;
